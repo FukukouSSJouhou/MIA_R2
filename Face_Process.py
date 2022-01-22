f7505502f7c02b9b7b1ddabbc86a65d21695b29e
kokkiemouse <kokkiemouse@fascode.net>
2022-01-22 22:37:28 +0900

    fx

--- Face_Process.py
+++ Face_Process.py
@@ -2,13 +2,15 @@ import os
 import subprocess
 import sys
 import wave
-
+import cv2
 
 def path_cutext(pathkun):
     pathkun22, extkun = os.path.splitext(os.path.basename(pathkun))
     return pathkun22
 class Face_Process():
     def __init__(self,filename,timedouga,logging_func):
+        self.voicefile = None
+        self.imgDIR_NAME = None
         self.endtime = None
         self.filename=filename
         self.timedouga=timedouga
@@ -24,6 +26,7 @@ class Face_Process():
         self.logging_func("Generated Wav File.")
         self.endtime=self.get_playtime()
         self.logging_func("The length of the video / audio file has been confirmed: {}".format(self.endtime))
+        self.target_img_select()
 
     def Make_audio(self):
         dir="./_audio/"
@@ -36,3 +39,14 @@ class Face_Process():
             fr=wr.getframerate()
             fn=wr.getnframes()
             return 1.0 * fn / fr
+    def target_img_select(self):
+        self.imgDIR_NAME = './FACE/temp_img/img_' + self.path_ONLY
+        if not os.path.exists(self.imgDIR_NAME):
+            os.makedirs(self.imgDIR_NAME)
+        self.logging_func(self.filename)
+        capture=cv2.VideoCapture(self.filename)
+        fps = capture.get(cv2.CAP_PROP_FPS)
+        print('fps :',fps);print('frame :',capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        
+        #self.endtime = capture.get(cv2.CAP_PROP_FRAME_COUNT) / fps
+        print('endtime :',self.endtime)
