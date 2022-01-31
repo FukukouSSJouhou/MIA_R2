31341fa0f15473f5897a804c754c055d45ca077f
kahenteikou <kokkiemouse@gmail.com>
2022-01-31 22:51:16 +0900

    修正
    関数ポインタぶっこんでた。アホすぎる。

--- Sentence_Process.py
+++ Sentence_Process.py
@@ -10,7 +10,7 @@ class Sentence_Process:
     def __init__(self,filename,logging_func,endtime,voicefile):
         self.filename=filename
         self.logging_func=logging_func
-        self.path_ONLY = path_cutexthanshin
+        self.path_ONLY = path_cutexthanshin(filename)
         self.endtime=endtime
         self.voicefile=voicefile
 
@@ -51,3 +51,29 @@ class Sentence_Process:
         self.logging_func('end')
         self.logging_func('{}s : {}\n'.format(len(starts_lengths), text))
 
+        # 切られた音声の位置（秒数）を記録
+        SENTENCEtimememo = Instance_sentence.Write_startendtimes()
+        self.logging_func('\033[34m' + 'Exported SENTENCEtimememo' +'\033[0m' )
+        # 切られた音声から文字起こしされた文章を記録
+        textslist = Instance_sentence.Write_texts()
+        self.logging_func('exported textslist')
+
+        #-------------------- Emotion dictionary part --------------------
+        for count in range(len(starts_lengths)+1):
+            print('='*30)
+            self.logging_func('==============================\n')
+            symbols, sentence, tokens = Instance_sentence.EmoRecognize(count)
+            print(sentence,'\n',tokens,'\nsymbols :',symbols)
+            self.logging_func('{}\n{}\n{}'.format(sentence, tokens, symbols))
+            SYMBOLs = Instance_sentence.EmoChange(symbols)
+            print('SYMBOLs :', SYMBOLs)
+            self.logging_func('SYMBOLs : {}\n'.format(SYMBOLs))
+            emoscount_list = Instance_sentence.CountEmo(SYMBOLs)
+            print('emoscount_list :', emoscount_list)
+            self.logging_func('emoscount_list : {}\n'.format(emoscount_list))
+        print('='*30)
+        self.logging_func('==============================\n')
+        SENTENCEemomemo = Instance_sentence.Write_emos()
+        self.logging_func('SENTENCEemomemoが出力されました\n')
+
+        return SENTENCEemomemo, SENTENCEtimememo, textslist
